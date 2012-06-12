4abb450c547c84c15af788d3503fe5ad52cea937
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-06-11 23:11:53 -0700

    doc: binding renames reflected in sample

--- sample.scm
+++ sample.scm
@@ -1,32 +1,39 @@
+(use chickmunk)
 
 
-(require-extension chickmunk)
+(if (not (fp= 0.5 (cp:moment-for-circle 1 0 1 cp:vzero)))
+    (signal "Cannot calculate moment for circle.
+Make sure your Chipmunk installation
+is configured to use floats and not doubles.
+Configure preprocessor with -DCP_USE_DOUBLES=0"))
 
+(define space (cp:space-new))
 
-(define space (cpSpaceNew))
-(print "space=" space)
+(cp:space-set-iterations space 10)
 
-(cpSpaceSetIterations space 10)
-(cpSpaceSetGravity space (cpv 0 -100))
+;; cp:v == cp:make-vect
+(cp:space-set-gravity space (cp:v 0 -100))
 
-                                        ;(cpSpaceAddShape space )
+(define static-body (cp:space-get-static-body space))
 
-(define d (make-cpVect 0 0))
-(%cpvadd d (make-cpVect 1 2) (cpv 3 4))
-(print "vector sum: " (cpVect-x d))
-
-
-(define static-body (cpSpaceGetStaticBody space))
-(print "static-body = " static-body)
-(cpSegmentShapeNew static-body
-                   (make-cpVect -320 -240)
-                   (make-cpVect -320 240)
+(cp:segment-shape-new static-body
+                   (cp:make-vect -320 -240)
+                   (cp:make-vect -320 240)
                    0)
-(define radius 15)
-(define body (cpSpaceAddBody space
-                             (cpBodyNew 10 (cpMomentForCircle 10 0 radius cpvzero))))
-(cpSpaceAddShape space (cpCircleShapeNew body radius cpvzero))
-(cpBodySetVel body (cpv 0 170))
 
-(define bodypos (cpBodyGetPos body))
-(print bodypos " @ " (cpVect-x bodypos) "," (cpVect-y bodypos))
+(define radius 15)
+(define body (cp:space-add-body space
+                                (cp:body-new 10 (cp:moment-for-circle 10 ;; mass
+                                                                      0 ;; inner radius
+                                                                      radius ;; outer radius
+                                                                      (cp:v 0 0) ;; offset
+                                                                      ))))
+
+(cp:space-add-shape space (cp:circle-shape-new body radius cp:vzero))
+(cp:body-set-vel body (cp:v 0 170))
+
+(cp:space-step space (/ 1 60))
+
+(let ([bodypos (cp:body-get-pos body)])
+  ;; you should see body's position altered by gravity
+  (print bodypos " @ " (cp:vect-x bodypos) ", " (cp:vect-y bodypos)))
