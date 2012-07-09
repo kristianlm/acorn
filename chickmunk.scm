f30f14d562e416d5cb3114434ff3529ab119c46e
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-07-09 01:44:49 -0700

    shape properties: using properties macro and renamed from shape-info shape-properties

--- chickmunk.scm
+++ chickmunk.scm
@@ -8,6 +8,9 @@
 
 #>
 #include <chipmunk/chipmunk.h>
+
+// for declarations of cpCircleShapeSetRadius etc
+#include <chipmunk/chipmunk_unsafe.h>
 <#
 
 (bind-rename/pattern "^cp" "")
@@ -164,40 +167,84 @@
                       (,(foreign-value "CP_POLY_SHAPE" int) . poly)
                       (,(foreign-value "CP_NUM_SHAPES" int) . num-shapes))))))
 
-(define (poly-shape-get-verts shape)
+(define (poly-shape-get-vertices shape)
   (map (compose
         vect->list
         (cut poly-shape-get-vert shape <>))
        (iota (poly-shape-get-num-verts shape))) )
 
-(define (shape-info shape)
-    (let* ([l vect->list]
-           [type (shape-get-type shape)]
-           [shape-info-all
-            (lambda ()
-              `((type ,type)
-                (sensor ,(shape-get-sensor shape))
-                (elasicity ,(shape-get-elasticity shape))
-                (friction ,(shape-get-friction shape))
-                (surface-velocity ,(l (shape-get-surface-velocity shape)))
-                (user-data ,(shape-get-user-data shape))
-                (collision-type ,(shape-get-collision-type shape))
-                (group ,(shape-get-group shape))
-                (layers ,(sprintf "~B" (shape-get-layers shape)))))]
-           [shape-info-circle
-            (lambda ()
-              `((offset ,(vect->list (circle-shape-get-offset shape)))
-                (radius ,(circle-shape-get-radius shape))))]
-           [shape-info-poly
-            (lambda ()
-              `((vertices ,(poly-shape-get-verts shape))))]
-           [shape-info-segment
-            (lambda ()
-              `())])
-      (append (shape-info-all) (case type
-                                 ([poly] (shape-info-poly))
-                                 ([circle] (shape-info-circle))
-                                 ([segment] (shape-info-segment))))))
+;; TODO
+(define (poly-shape-set-vertices shape)
+ #f)
+
+(define (segment-shape-get-endpoints shape)
+  (assert (eq? (shape-get-type shape) 'segment) "shape is not of type segment")
+  (list (vect->list (segment-shape-get-a shape))
+        (vect->list (segment-shape-get-b shape))))
+
+;; convert from three arguments to two argument signature like the rest
+(define segment-shape-set-endpoints
+  (let ([foreign-set-endpoints segment-shape-set-endpoints])
+    (lambda (shape new-value)
+      (foreign-set-endpoints shape
+                             (list->vect (car new-value))
+                             (list->vect (cadr new-value))))))
+
+(define-values [shape-properties shape-properties-set!]
+  ;; define hidden getter and setter procedures for each
+  ;; shape type. then shape-properties and shape-properties-set!
+  ;; will dispatch
+  (let ()
+    (define-info-supporters
+      common-shape-properties common-shape-properties-set!
+      shape-get- shape-set-
+      (  (type #f #f #f "cannot change shape type")
+         sensor
+         elasticity
+         friction
+         (surface-velocity vect->list list->vect)
+         user-data
+         collision-type
+         group
+         (layers (lambda (l) (sprintf "~B" l)))))
+
+    (define-info-supporters
+      circle-shape-properties circle-shape-properties-set!
+      circle-shape-get- circle-shape-set-
+      (  (offset vect->list list->vect)
+         radius))
+
+    (define-info-supporters
+      poly-shape-properties poly-shape-properties-set!
+      poly-shape-get- poly-shape-set-
+      (  (vertices #f #f #f "no setter for segment vertices yet")  ))
+
+    (define-info-supporters
+      segment-shape-properties segment-shape-properties-set!
+      segment-shape-get- segment-shape-set-
+      (  endpoints  ))
+
+    (define (shape-properties shape)
+      (append (common-shape-properties shape)
+              (case (shape-get-type shape)
+                ([circle] (circle-shape-properties shape))
+                ([poly] (poly-shape-properties shape))
+                ([segment] (segment-shape-properties shape))
+                (else '((error "don't know this shape type"))))))
+
+    (define (shape-properties-set! shape new-value)
+      (append
+       (common-shape-properties-set! shape new-value)
+       (case (shape-get-type shape)
+         ([circle] (circle-shape-properties-set! shape new-value))
+         ([poly] (poly-shape-properties-set! shape new-value))
+         ([segment] (segment-shape-properties-set! shape new-value))
+         (else '((error "shape type unknown"))))))
+
+    (values
+     shape-properties
+     shape-properties-set!)))
+
 
 (include "chickmunk-callback.scm")
 
