6531f1d52bc3d74a1280ba21db34b472a374a631
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-07-16 19:31:30 -0700

    sample: shortened sample using new node features

--- sample.scm
+++ sample.scm
@@ -1,41 +1,20 @@
 (use chickmunk)
 
+(define space
+  (nodes->space `(space ()
+                        (body
+                         ()
+                         (circle (radius 1))))))
 
-(if (not (fp= 0.5 (cp:moment-for-circle 1 0 1 cp:vzero)))
-    (signal "Cannot calculate moment for circle.
-Make sure your Chipmunk installation
-is configured to use floats and not doubles.
-Configure preprocessor with -DCP_USE_DOUBLES=0"))
+;; give our world a little gravity
+(space-set-gravity space (v 0 -1))
 
-(define space (cp:space-new))
+;; run our simulation for a while,
+;; letting our ball fall
+(do ((i 0 (add1 i)))
+    ((> i 100))
+  (space-step space (/ 1 120)))
 
-(cp:space-set-iterations space 10)
-
-;; cp:v == cp:make-vect
-(cp:space-set-gravity space (cp:v 0 -100))
-
-(define static-body (cp:space-get-static-body space))
-
-(cp:segment-shape-new static-body
-                   (cp:make-vect -320 -240)
-                   (cp:make-vect -320 240)
-                   0)
-
-(define radius 15)
-(define body (cp:space-add-body space
-                                (cp:body-new 10 (cp:moment-for-circle 10 ;; mass
-                                                                      0 ;; inner radius
-                                                                      radius ;; outer radius
-                                                                      (cp:v 0 0) ;; offset
-                                                                      ))))
-
-(cp:space-add-shape space (cp:circle-shape-new body radius cp:vzero))
-(cp:body-set-vel body (cp:v 0 170))
-
-(cp:space-step space (/ 1 60))
-
-(let ([bodypos (cp:body-get-pos body)])
-  ;; you should see body's position altered by gravity
-  (print bodypos " @ " (cp:vect-x bodypos) ", " (cp:vect-y bodypos)))
-
-(cp:space-free space)
+;; y-coordinate of pos and vel should be altered by gravity
+(pp (space->nodes space))
+(space-free space)
