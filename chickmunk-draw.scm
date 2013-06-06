e8bce5e7d191da21ea8df63c3a3687b170133f1b
Kristian Lein-Mathisen <kristianlein@gmail.com>
2013-06-07 01:49:35 +0200

    build fix, chickmunk-draw: use foreign-transformer from chickmunk module

--- chickmunk-draw.scm
+++ chickmunk-draw.scm
@@ -8,8 +8,16 @@
 #include "ChipmunkDebugDraw.h"
 <#
 
+(begin-for-syntax
+ (import chicken)
+ (include "chickmunk-transformer.scm")
+ (define chickmunk-draw#chickmunk-transformer chickmunk#chickmunk-transformer))
+
+;; hack! same as in chickmunk-bind.scm
+(define chickmunk-transformer (void))
+
 ;(bind-file "chipmunk.h")
-(bind-options default-renaming: "")
+(bind-options default-renaming: "" foreign-transformer: chickmunk-transformer)
 ;;chickmunk-draw#chipmunk-debug-draw-segment
 (bind-rename/pattern "^chipmunk-debug-" "")
 (bind-file "ChipmunkDebugDraw.h")
